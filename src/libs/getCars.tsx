a68c938a2408b3934ebd8aa3c5e2fe03ac57071e
agehcx <chega.malee@gmail.com>
2024-03-26 00:10:59 +0700

    duh duh

--- src/libs/getCars.tsx
+++ src/libs/getCars.tsx
@@ -1,7 +1,7 @@
 export default async function getCars() {
 
     // add timeout for loading delay testing
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
     
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/cars`, { next: {tags:['cars']}}) 
     
