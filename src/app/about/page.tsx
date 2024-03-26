0d9a072f912bb4aed3a956106c92342dfbf6a4b9
Daniel Samae <danielsamae555@gmail.com>
2024-03-26 22:23:14 +0700

    Mafia

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
 export default function About() {
     return (
-        <main>
-            <div>About Us</div>
+        <main className="flex flex-col items-center">
+            <div className="text-center">About Us</div>
+            <div className="text-center">Mafia Hat Yai Hod Makmak</div>
+            <img src="/img/mafia.jpg" alt="Mafia Hat Yai" className="mt-10 w-96" />
         </main >
     );
 }
